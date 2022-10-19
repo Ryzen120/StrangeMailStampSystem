5934e8322ad85b1c491ae8e4a4e36318c8f6aaa8
Amandeep Singh <amandeep.singh231998@gmail.com>
2022-10-19 19:27:39 -0400

    autoroller
    added function

--- AutoRoller.cs
+++ AutoRoller.cs
@@ -8,7 +8,64 @@ namespace StrangeMailStampSystem
 {
     class AutoRoller
     {
+        private List<string> pnRollers;
+        private List<string> psRollers;
+        private Dictionary<string, int> pssRollers; 
+        private Dictionary<string, int> pnEndRolls;
+        private Dictionary<string, int> psEndRolls;
+        private Dictionary<string, int> allRolls;
+        StrangeMailStampSystem gui;
 
+        public AutoRoller(StrangeMailStampSystem gui)
+        {
+            pnRollers = Globals.nRollers;
+            psRollers = Globals.sRollers;
+            pssRollers = Globals.ssRollers;
+            this.gui = gui;
+        }
+
+        private void dWinner()
+        {
+            gui.UpdateLogs("a");
+        }
+
+        private Dictionary<string, int> nRoll()
+        {
+            Random rnd = new Random();
+            foreach (string roller in pnRollers)
+            {
+                pnEndRolls.Add(roller, rnd.Next(1,100));
+            }
+
+            return pnEndRolls;
+        }
+        private Dictionary<string, int> sRoll()
+        {
+            Random rnd = new Random();
+
+            foreach (var item in pssRollers)
+            {
+                psEndRolls.Add(item.Key, item.Value + rnd.Next(1, 100));
+            }
+            return psEndRolls;
+        }
+
+        private Dictionary<string, int> megaDic()
+        {
+            
+            foreach(var item in pnEndRolls)
+            {
+                allRolls.Add(item.Key,item.Value);
+
+            }
+            foreach (var item in psEndRolls)
+            {
+                allRolls.Add(item.Key, item.Value);
+            }
+
+            return allRolls;
+
+        }
 
     }
 }
--- Globals.cs
+++ Globals.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace StrangeMailStampSystem
 {
-    class Globals
+    public class Globals
     {
 
         public static Dictionary<string, FuncInfo> FunctionDictionary { get; set; }
+
+        public static string gWinner;
+
+        public static List<string> nRollers;
+
+        public static List<string> sRollers;
+
+        public static Dictionary<string, int> ssRollers;
 
     }
 }
